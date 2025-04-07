f8410d92e3c218769e6423e9c5299e86432ce3a0
Shashi sah <sah.shashi2003@gmail.com>
2025-04-07 14:35:10 +0530

    set default base URL in next.config.ts for PR failure in github workflow

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:7321";
     if (!baseUrl) {
       throw new Error('NEXT_PUBLIC_BASE_URL environment variable is not defined');
     }
